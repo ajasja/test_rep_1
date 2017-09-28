672c9e6e21a983d1eb874081ec452b68f278b9a8
janaaupic <jana.aup@gmail.com>
2017-09-28 12:32:01 +0200

    Changed path to file segments.xlsx, test test_make_json.py should now work

--- cocopod/make_json.py
+++ cocopod/make_json.py
@@ -103,8 +103,8 @@ def generate_json(name, entire_sequence, segments_str, pairs, out_name=None):
         
 #determining the heptad position of the starting residue in a segment
         path = u.relative_to(__file__,'')
-        seg_to_seq = sa.seq_to_seq_map(os.path.join(path, 'segments.xlsx'))
-        pair_1_seq = seg_to_seq[seg_name].replace(" ","")
+        seg_to_seq = sa.seq_to_seq_map(os.path.join(path, '../examples/TET/segments.xlsx'))
+        pair_1_seq = seg_to_seq[seg_name.partition("_")[0]].replace(" ","")
         template_start, target_start = u.align(pair_1_seq, seq, 6, 6)
         whole_register = ['c','d','e','f','g','a','b']
         register = whole_register[template_start%7]        
